0db810128de368990061697462ec952704e650b9
s23010843 <s23010843@ousl.lk>
2025-11-19 11:39:20 +0530

    react

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import vue from '@astrojs/vue';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [vue()]
+  integrations: [vue(), react()]
 });
