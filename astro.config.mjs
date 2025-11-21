7e02d91222dc12e8576d0dc3ad6b825a1fec10ef
s23010843 <s23010843@ousl.lk>
2025-11-21 10:21:06 +0530

    svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,8 @@ import sitemap from '@astrojs/sitemap';
 
 import analogjsangular from '@analogjs/astro-angular';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://s23010843.github.io',
@@ -20,5 +22,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [vue(), react(), sitemap(), analogjsangular()]
+  integrations: [vue(), react(), sitemap(), analogjsangular(), svelte()]
 });
