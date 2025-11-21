6970ee2e8f2b712075dc842bbc81f76d9e5e933b
s23010843 <s23010843@ousl.lk>
2025-11-21 07:37:20 +0530

    React Re-Test

--- src/components/ReactWelcome.jsx
+++ src/components/ReactWelcome.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const ReactWelcome = () => {
-  return (
-    <h1 style={{ color: 'blue', fontSize: '24px', textAlign: 'center' }}>
-      React is working!
-    </h1>
+export default function ReactWelcome() {
+  return React.createElement(
+    'h1',
+    { style: { color: 'blue', fontSize: '24px', textAlign: 'center' } },
+    'React is working!'
   );
-};
-
-export default ReactWelcome;
+}
